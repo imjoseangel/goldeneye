46a54abaa07f1506215b6552439730c7cbd40129
imjoseangel <josea.munoz@gmail.com>
2024-06-01 21:12:05 +0200

    add exceptions

--- util/getuas.py
+++ util/getuas.py
@@ -21,14 +21,17 @@ soup = BeautifulSoup(html_doc)
 
 liste = soup.find(id='liste')
 
-uas = liste.find_all('li')
+try:
 
-if len(uas) <= 0:
+    uas = liste.find_all('li')
+
+    if len(uas) <= 0:
+        print("No UAs Found. Are you on http://www.useragentstring.com/ lists?")
+        sys.exit(1)
+
+    for ua in uas:
+        ua_string = ua.get_text()
+        ua_string = ua_string.strip(' \t\n\r')
+        print(ua_string)
+except (AttributeError, NameError):
     print("No UAs Found. Are you on http://www.useragentstring.com/ lists?")
-    sys.exit(1)
-
-
-for ua in uas:
-    ua_string = ua.get_text()
-    ua_string = ua_string.strip(' \t\n\r')
-    print(ua_string)
